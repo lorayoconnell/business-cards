6b5bb7397731387c0ee6a714a3f23ac3222fba3b
Loray O'Connell <lorayoconnell@me.com>
2019-11-09 08:56:07 -0800

    updated app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CardsModule } from './cards/cards.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { WebcamComponent } from './webcam/webcam.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    WebcamComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardsModule,
+    AuthModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
